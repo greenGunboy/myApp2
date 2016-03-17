6bd97f7d367fc7ffed99bfb4865b5ff5b25212c1
Tsubasa Takahashi <tsubasatakahashi47@gmail.com>
2016-03-17 19:51:10 +0800

    no message

--- myApp2/GameScene.swift
+++ myApp2/GameScene.swift
@@ -8,38 +8,91 @@
 
 import SpriteKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
+    
+    var myTexture = SKTexture(imageNamed: "maru.png")
+    var user = SKSpriteNode(imageNamed: "")
+    var startBtn = SKSpriteNode(imageNamed: "maru.png")
+    let redCategory: UInt32 = 0x1 << 0
+    let greenCategory: UInt32 = 0x1 << 1
+    
     override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!"
-        myLabel.fontSize = 45
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
         
-        self.addChild(myLabel)
+        self.physicsWorld.gravity = CGVector(dx: 0, dy: -3.0)
+        self.physicsWorld.contactDelegate = self
+        self.physicsBody = SKPhysicsBody(edgeLoopFromRect: CGRect(x: 0, y: 0, width: size.width, height: size.height))
+        
+        user.physicsBody!.categoryBitMask = redCategory
+        user.physicsBody!.contactTestBitMask = greenCategory
+        
+//        var a:SKSpriteNode = SKSpriteNode(color: SKColor.blackColor(), size: CGSizeMake(15, 80))
+//        a.size = CGSizeMake(150, 150)
+//        a.position = CGPoint(x: 500, y: 300)
+//        a.physicsBody = SKPhysicsBody(rectangleOfSize: CGSize(width: 185, height: 205))
+        
+        startBtn.size = CGSizeMake(100, 100)
+        startBtn.position = CGPoint(x: self.frame.width / 2, y: 1200)
+        
+        user.size = CGSizeMake(120, 120)
+        
+        self.addChild(user)
+        self.addChild(startBtn)
+        
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-       /* Called when a touch begins */
-        
         for touch in touches {
+            
             let location = touch.locationInNode(self)
+            let toucheNode = self.nodeAtPoint(location)
             
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
+            if toucheNode == startBtn {
+                for var i = 0; i < 100; i++ {
+                    var sprite = SKSpriteNode(texture: myTexture)
+                    sprite.position = startBtn.position
+                    sprite.size = CGSizeMake(20, 20)
+                    sprite.physicsBody = SKPhysicsBody(circleOfRadius: 10)
+                    sprite.physicsBody?.friction = 0.0
+                    sprite.physicsBody!.restitution = 0.2
+                    sprite.physicsBody!.velocity = CGVectorMake(500, 400)
+                    self.addChild(sprite)
+                }
+            }
+        }
+    }
+    
+//    衝突時の処理
+    func didBeginContact(contact: SKPhysicsContact) {
+        
+        var firstBody, secondBody: SKPhysicsBody
+        
+        // firstを赤、secondを緑とする。
+        if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask {
+            firstBody = contact.bodyA
+            secondBody = contact.bodyB
+        } else {
+            firstBody = contact.bodyB
+            secondBody = contact.bodyA
+        }
+        
+        // 赤と緑が接したときの処理。
+        if firstBody.categoryBitMask && user != 0 && secondBody.categoryBitMask & myTexture != 0 {
+                secondBody.node!.removeFromParent()
+        }
+
+    }
+    
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        for touch: AnyObject in touches {
             
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
+            let location = touch.locationInNode(self)
+            user.position = location
             
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
         }
     }
    
     override func update(currentTime: CFTimeInterval) {
-        /* Called before each frame is rendered */
+        
     }
+    
 }
