a1c6cbf0abf3754609bc81379032bcf6cc06edc4
Tsubasa Takahashi <tsubasatakahashi47@gmail.com>
2016-03-17 22:18:48 +0800

    no message

--- myApp2/GameScene.swift
+++ myApp2/GameScene.swift
@@ -15,28 +15,39 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     var startBtn = SKSpriteNode(imageNamed: "maru.png")
     let redCategory: UInt32 = 0x1 << 0
     let greenCategory: UInt32 = 0x1 << 1
+    let path = NSBundle.mainBundle().pathForResource("MyParticle", ofType: "sks")
+    var contactCount = 0
+    var contactCountLabel: SKLabelNode?
     
     override func didMoveToView(view: SKView) {
         
-        self.physicsWorld.gravity = CGVector(dx: 0, dy: -3.0)
+        self.physicsWorld.gravity = CGVector(dx: 0, dy: -1.8)
         self.physicsWorld.contactDelegate = self
         self.physicsBody = SKPhysicsBody(edgeLoopFromRect: CGRect(x: 0, y: 0, width: size.width, height: size.height))
         
+        user.physicsBody = SKPhysicsBody(rectangleOfSize: CGSizeMake(50, 50))
+        user.physicsBody!.affectedByGravity = false
+        user.physicsBody!.dynamic = false
         user.physicsBody!.categoryBitMask = redCategory
         user.physicsBody!.contactTestBitMask = greenCategory
-        
-//        var a:SKSpriteNode = SKSpriteNode(color: SKColor.blackColor(), size: CGSizeMake(15, 80))
-//        a.size = CGSizeMake(150, 150)
-//        a.position = CGPoint(x: 500, y: 300)
-//        a.physicsBody = SKPhysicsBody(rectangleOfSize: CGSize(width: 185, height: 205))
         
         startBtn.size = CGSizeMake(100, 100)
         startBtn.position = CGPoint(x: self.frame.width / 2, y: 1200)
         
         user.size = CGSizeMake(120, 120)
-        
+        user.position = CGPoint(x: self.frame.width / 2, y: 100)
         self.addChild(user)
         self.addChild(startBtn)
+        
+        let contactCountLabel = SKLabelNode(fontNamed: "Chalkduster")
+        contactCountLabel.position = CGPoint(x: size.width * 0.92, y: size.height * 0.92)
+        contactCountLabel.fontSize = 40
+        contactCountLabel.horizontalAlignmentMode = .Right
+        contactCountLabel.fontColor = UIColor.whiteColor()
+        contactCountLabel.text = "\(contactCount) / 1000"
+        addChild(contactCountLabel)
+        self.contactCountLabel = contactCountLabel
+
         
     }
     
@@ -47,14 +58,14 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             let toucheNode = self.nodeAtPoint(location)
             
             if toucheNode == startBtn {
-                for var i = 0; i < 100; i++ {
+                for var i = 0; i < 101; i++ {
                     var sprite = SKSpriteNode(texture: myTexture)
                     sprite.position = startBtn.position
                     sprite.size = CGSizeMake(20, 20)
                     sprite.physicsBody = SKPhysicsBody(circleOfRadius: 10)
                     sprite.physicsBody?.friction = 0.0
                     sprite.physicsBody!.restitution = 0.2
-                    sprite.physicsBody!.velocity = CGVectorMake(500, 400)
+                    sprite.physicsBody!.velocity = CGVectorMake(100, 400)
                     self.addChild(sprite)
                 }
             }
@@ -66,7 +77,6 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         var firstBody, secondBody: SKPhysicsBody
         
-        // firstを赤、secondを緑とする。
         if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask {
             firstBody = contact.bodyA
             secondBody = contact.bodyB
@@ -75,11 +85,21 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             secondBody = contact.bodyA
         }
         
-        // 赤と緑が接したときの処理。
-        if firstBody.categoryBitMask && user != 0 && secondBody.categoryBitMask & myTexture != 0 {
-                secondBody.node!.removeFromParent()
+        if firstBody.categoryBitMask & redCategory != 0 && secondBody.categoryBitMask & greenCategory != 0 {
+            secondBody.node!.removeFromParent()
+            contactCount++
         }
-
+        
+        var particle = NSKeyedUnarchiver.unarchiveObjectWithFile(path!) as! SKEmitterNode
+        particle.position = contact.contactPoint
+        particle.numParticlesToEmit = 100 // 何個、粒を出すか。
+        particle.particleBirthRate = 200 // 一秒間に何個、粒を出すか。
+        particle.particleSpeed = 80 // 粒の速度
+        particle.xAcceleration = 0
+        particle.yAcceleration = 0 // 加速度
+        self.addChild(particle)
+        
+        contactCountLabel?.text = "\(contactCount) / 1000"
     }
     
     override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
